36d313e09943a1b6bc4c56c27141f951de74af51
Anziq Majid <144418275+Majidbrohi@users.noreply.github.com>
2024-10-22 15:05:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var toggleSkillsButton = document.getElementById('toggleSkillsButton');
+var skillsSection = document.getElementById('skills');
+toggleSkillsButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+        toggleSkillsButton.textContent = 'Hide Skills';
+    }
+    else {
+        skillsSection.style.display = 'none';
+        toggleSkillsButton.textContent = 'Show Skills';
+    }
+});
